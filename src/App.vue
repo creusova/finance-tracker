c18d99f6403f6a5afa7b25390bd7f70c14bd6219
Creusova <creusova@mail.ru>
2026-05-12 14:36:43 +0300

    move transaction table to component

--- src/App.vue
+++ src/App.vue
@@ -2,81 +2,65 @@
 import { ref, computed, watch } from 'vue'
 import BalanceBlock from './components/balanceBlock.vue'
 import TransactionForm from './components/transactionForm.vue'
+import TransactionTable from './components/transactionTable.vue'
 
-  const transactions = ref([
-    {
-      id: 1,
-      type: 'expense',
-      date: '2026-04-10',
-      sum: 12,
-      category: 'Food',
-      comment: ''
-    },
-    {
+const transactions = ref([
+  {
+    id: 1,
+    type: 'expense',
+    date: '2026-04-10',
+    sum: 12,
+    category: 'Food',
+    comment: '',
+  },
+  {
     id: 2,
     type: 'income',
     date: '2026-04-10',
     sum: 12000,
     category: 'Work',
-    comment: 'new'
-    }
-  ])
+    comment: 'new',
+  },
+])
 
+const income = computed(() => {
+  return transactions.value
+    .filter((t) => t.type === 'income')
+    .reduce((total, t) => total + t.sum, 0)
+})
 
-  const income = computed(() =>{
-    return transactions.value
-    .filter(t => t.type ==='income')
-    .reduce((total,t)=>total + t.sum,0)
-  })
+const expense = computed(() => {
+  return transactions.value
+    .filter((t) => t.type === 'expense')
+    .reduce((total, t) => total + t.sum, 0)
+})
 
-  const expense = computed(() =>{
-    return transactions.value
-    .filter(t => t.type ==='expense')
-    .reduce((total,t)=>total + t.sum,0)
-  })
+const balance = computed(() => {
+  return income.value - expense.value
+})
 
-  const balance = computed(() =>{
-    return income.value- expense.value
-  })
+function addTransaction(transaction) {
+  transactions.value.push(transaction)
+}
 
-  function addTransaction(transaction){
-    transactions.value.push(transaction)
-  }
-
-  function selectedConsole(){
-    console.log(selected.value)
-    console.log(transactionDate.value)
-  }
-
-
+function selectedConsole() {
+  console.log(selected.value)
+  console.log(transactionDate.value)
+}
 </script>
-
-
 
 <template>
   <header>
-   <h1>Finance Traker</h1>
+    <h1>Finance Traker</h1>
   </header>
 
   <main>
-    <BalanceBlock 
-      :balance = "balance"
-      :income = "income"
-      :expense = "expense"/>
+    <BalanceBlock :balance="balance" :income="income" :expense="expense" />
 
-    <TransactionForm 
-      @add-transaction="addTransaction"/>
+    <TransactionForm @add-transaction="addTransaction" />
 
-   
-   <div>
-      <p v-for="transaction in transactions" :key="transaction.id">
-        {{ transaction.type }} {{ transaction.date }} {{ transaction.sum }} {{ transaction.category }} {{ transaction.comment }}
-      </p>
-   </div>
-  
+    <TransactionTable :transactions="transactions" />
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/transactionTable.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  transactions: {
+    type: Array,
+    default: () => [],
+  },
+})
+</script>
+
+<template>
+  <div>
+    <p v-for="transaction in transactions" :key="transaction.id">
+      {{ transaction.type }} {{ transaction.date }} {{ transaction.sum }}
+      {{ transaction.category }} {{ transaction.comment }}
+    </p>
+  </div>
+</template>
