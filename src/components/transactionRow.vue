bdde091dc6e494300d864b4e26b9c2cb2641391b
Creusova <creusova@mail.ru>
2026-05-15 13:06:28 +0300

    feat: add component transaction row

---
+++ src/components/transactionRow.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  transaction: {
+    type: Object,
+    default: () => {},
+  },
+})
+</script>
+
+<template>      
+    <tr>
+      <td>{{ transaction.type }}</td>
+      <td>{{ transaction.date }}</td>
+      <td>{{ transaction.sum }}</td>
+      <td>{{ transaction.category }}</td>
+      <td>{{ transaction.comment }}</td>
+    </tr>
+</template>
--- src/components/transactionTable.vue
+++ src/components/transactionTable.vue
@@ -1,4 +1,6 @@
 <script setup>
+import TransactionRow from './transactionRow.vue'
+
 defineProps({
   transactions: {
     type: Array,
@@ -21,15 +23,16 @@ defineProps({
       </tr>
     </thead>
     <tbody>              
-    <tr v-for="transaction in transactions" :key="transaction.id">
-      <td>{{ transaction.type }}</td>
+      <!-- <tr v-for="transaction in transactions" :key="transaction.id"> -->
+      <!-- <td>{{ transaction.type }}</td>
       <td>{{ transaction.date }}</td>
       <td>{{ transaction.sum }}</td>
       <td>{{ transaction.category }}</td>
-      <td>{{ transaction.comment }}</td>
-    </tr>
-  </tbody>
-</table>
+      <td>{{ transaction.comment }}</td> -->
+        <TransactionRow v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+      <!-- </tr> -->
+    </tbody>
+    </table>
 
   </div>
 </template>
